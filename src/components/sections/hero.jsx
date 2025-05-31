c5304da628c8aa78dc9dbc07d82450abcc4efa87
beniamin-hekimian <penig300404@gmail.com>
2025-05-31 16:51:29 +0300

    hide title stars

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -10,7 +10,7 @@ export default function Hero() {
     <section id="hero" className="my-12">
       <div className="container mx-auto px-4 lg:px-12 grid grid-cols-1 lg:grid-cols-2 items-center gap-8">
         <div>
-          <h1 className="text-3xl md:text-4xl font-semibold"><BsStars className="inline" /><span className="text-green-500">Beniamin</span> Hekimian</h1>
+          <h1 className="text-3xl md:text-4xl font-semibold"><BsStars className="hidden md:inline" /><span className="text-green-500">Beniamin</span> Hekimian</h1>
           <h2 className="text-2xl md:text-3xl font-semibold mt-2">I&apos;m a <Typewriter /></h2>
           <p className="mt-4">This is my official portfolio website, where I showcase my skills in web development, photography, and video editing. Explore my projects to see how I bring creativity and technical expertise together!</p>
           <Button className="my-4 hover:cursor-pointer">Download CV<Download /></Button>
